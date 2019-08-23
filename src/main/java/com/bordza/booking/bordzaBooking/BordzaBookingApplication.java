eec17592f5b5e8f49fd4c006622b9dfd5bfbcb4f
manza33 <bourgois.laetitia@gmail.com>
2019-08-23 16:51:08 +0200

    first commit

---
+++ src/main/java/com/bordza/booking/bordzaBooking/BordzaBookingApplication.java
@@ -0,0 +1,13 @@
+package com.bordza.booking.bordzaBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordzaBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordzaBookingApplication.class, args);
+	}
+
+}
