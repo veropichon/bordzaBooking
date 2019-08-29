b1f233bac285cfc151b7f07dadd13bb2069c23d9
manza33 <bourgois.laetitia@gmail.com>
2019-08-29 11:31:22 +0200

    Correction Jpa id

--- src/main/java/com/bordza/booking/bordzaBooking/domain/UserEntity.java
+++ src/main/java/com/bordza/booking/bordzaBooking/domain/UserEntity.java
@@ -3,10 +3,7 @@ package com.bordza.booking.bordzaBooking.domain;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 //@Data
@@ -18,6 +15,7 @@ public class UserEntity {
 
     //@Getter
     @Id
+    @Column(name = "usr_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long usrId;
 
@@ -32,6 +30,12 @@ public class UserEntity {
     }
 
     public UserEntity(String usrLogin, @Size(min = 4, max = 15) String usrPwd) {
+        this.usrLogin = usrLogin;
+        this.usrPwd = usrPwd;
+    }
+
+    public UserEntity(Long usrId, String usrLogin, @Size(min = 4, max = 15) String usrPwd) {
+        this.usrId = usrId;
         this.usrLogin = usrLogin;
         this.usrPwd = usrPwd;
     }
--- src/main/java/com/bordza/booking/bordzaBooking/services/service.java
+++ src/main/java/com/bordza/booking/bordzaBooking/services/service.java
@@ -1,4 +1,5 @@
 package com.bordza.booking.bordzaBooking.services;
 
 public class service {
+
 }
