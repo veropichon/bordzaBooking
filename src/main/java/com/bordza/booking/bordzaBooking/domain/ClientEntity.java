8a7e830c39632b8619002c5d64c14ed5e557bb39
manza33 <bourgois.laetitia@gmail.com>
2019-09-04 11:28:58 +0200

    Ok default

--- src/main/java/com/bordza/booking/bordzaBooking/domain/ClientEntity.java
+++ src/main/java/com/bordza/booking/bordzaBooking/domain/ClientEntity.java
@@ -1,6 +1,7 @@
 package com.bordza.booking.bordzaBooking.domain;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.sql.Date;
@@ -19,18 +20,55 @@ public class ClientEntity {
     @ManyToOne
     private LevelEntity level;
 
+    @Basic
+    @Column(nullable = false, length = 100)
     private String cliFirstname;
+
+    @Basic
+    @Column(nullable = false, length = 100)
     private String cliLastname;
+
+    @Basic
+    @Column(nullable = false, length = 10)
     private String cliPhone;
+
+    @Basic
+    @Column(nullable = false, length = 5)
     private Integer cliZipcode;
+
+    @Basic
+    @Column(nullable = false, length = 100)
     private String cliCity;
+
+    @Basic
+    @Column(length = 3)
     private Integer cliWeight;
+
+    @Basic
+    @Column(length = 3)
     private Integer cliHeight;
+
+    @DateTimeFormat
     private Date cliBirthdate;
+
+    @Basic
+    @Column(length = 100)
     private String cliTutorFirstname;
+
+    @Basic
+    @Column(length = 100)
     private String cliTutorLastname;
+
+    @Basic
+    @Column(length = 100)
     private String cliTutorEmail;
+
+    @Basic
+    @Column(length = 100)
     private String cliTutorPhone;
+
+    @Basic
+    @Column(length = 255)
     private String cliComment;
 
     @Column(nullable = false)
@@ -43,8 +81,7 @@ public class ClientEntity {
      * Display Default values
      * @return client entity with default values
      */
-    public static ClientEntity defaultValue() {
-        ClientEntity clientEntity = new ClientEntity();
+    public static ClientEntity defaultValue(ClientEntity clientEntity) {
         clientEntity.cliValidated = false;
         clientEntity.cliDeleted = false;
         return clientEntity;
--- src/main/java/com/bordza/booking/bordzaBooking/services/ClientService.java
+++ src/main/java/com/bordza/booking/bordzaBooking/services/ClientService.java
@@ -7,8 +7,10 @@ import com.bordza.booking.bordzaBooking.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.util.PatternMatchUtils;
+
+import java.sql.Date;
 
 
 @Slf4j
@@ -21,6 +23,8 @@ public class ClientService {
     @Autowired
     ClientRepository clientRepository;
 
+
+
     /**
      * Exception check when @POST from inscription form
      * @param userEntity user input form inscription
@@ -29,22 +33,58 @@ public class ClientService {
      */
     public void saveClient(UserEntity userEntity, ClientEntity clientEntity) throws IllegalArgumentException {
 
-        String clientlvl = clientEntity.getLevel().getLevClientLabel();
+        String cliFirstname = StringUtils.trim(clientEntity.getCliFirstname());
+        String cliLastname = StringUtils.trim(clientEntity.getCliLastname());
+        String cliPhone = StringUtils.trim(clientEntity.getCliPhone());
+        Integer cliZipcode = clientEntity.getCliZipcode();
+        String cliCity = StringUtils.trim(clientEntity.getCliCity());
+        Integer cliWeight = clientEntity.getCliWeight();
+        Integer cliHeight = clientEntity.getCliHeight();
+        Date cliBirthdate = clientEntity.getCliBirthdate();
+        String cliTutorFirstname = StringUtils.trim(clientEntity.getCliTutorFirstname());
+        String cliTutorLastname = StringUtils.trim(clientEntity.getCliTutorLastname());
+        String cliTutorEmail = StringUtils.trim(clientEntity.getCliTutorEmail());
+        String cliTutorPhone = StringUtils.trim(clientEntity.getCliTutorPhone());
+        String cliComment = StringUtils.trim(clientEntity.getCliComment());
+        Long clientlvlId = clientEntity.getLevel().getLevId();
+
         String usrEmail = StringUtils.trim(userEntity.getUsrEmail());
         String usrPwd = userEntity.getUsrPwd();
 
-        log.info("User : " + userEntity.toString());
-        log.info("Client : " + clientEntity.toString());
+        /*final String Emailregex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        boolean PatternEmail = PatternMatchUtils.simpleMatch(Emailregex, usrEmail);
 
-        if (StringUtils.isBlank(usrEmail)) {
-            throw new IllegalArgumentException("user email is invalid");
+        final String PwdRegex = "((?=.*[a-z])(?=.*d)(?=.*[A-Z]).{6,16})";
+        boolean PatternPwd = PatternMatchUtils.simpleMatch(PwdRegex, usrPwd);
+
+        EmailValidator emailValidator = EmailValidator.getInstance();
+        valid = emailValidator.isValid("sandeep_giet@yahoo.com");
+
+        if (StringUtils.isBlank(usrEmail) && !PatternEmail && usrEmail.length() <= 6) {
+            throw new IllegalArgumentException("Votre email n'est pas valide");
         }
 
-        if(StringUtils.isEmpty(usrPwd) /* FIXME RULES */) {
-            throw new IllegalArgumentException("password doesn't comply with...");
+        if(StringUtils.isEmpty(usrPwd) && !PatternPwd) {
+           throw new IllegalArgumentException("Le mot de passe n'est pas valide. Vous devez utiliser au moins minimum 6 lettres, une majuscule et un nombre");
         }
+
+        if (StringUtils.isBlank((cliFirstname)) && PatternMatchUtils.simpleMatch("(\\d*)", cliFirstname)){
+            throw new  IllegalArgumentException("Votre Prénom ne peut pas contenir de chiffre");
+        }
+
+        if (StringUtils.isBlank((cliLastname)) && PatternMatchUtils.simpleMatch("\\d*", cliLastname)){
+            throw new  IllegalArgumentException("Votre nom ne peut pas contenir de chiffre");
+        }
+
+        if (StringUtils.isBlank((cliPhone)) && PatternMatchUtils.simpleMatch("([0-9]{2}){5}", cliLastname)){
+            throw new  IllegalArgumentException("Votre nom ne peut pas contenir de chiffre");
+        }*/
+
 
         // TODO verif!
+
+        log.info("User : " + userEntity.toString());
+        log.info("Client : " + clientEntity.toString());
 
         userRepository.save(userEntity);
 
