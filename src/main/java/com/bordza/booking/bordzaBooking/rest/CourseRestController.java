e6eaf9e95ed0b39a3b9e1b30f7a1256ad105b28d
Veronique Pichon <name>
2019-09-10 14:58:22 +0200

    recup transaction Proposition cours

--- src/main/java/com/bordza/booking/bordzaBooking/rest/CourseRestController.java
+++ src/main/java/com/bordza/booking/bordzaBooking/rest/CourseRestController.java
@@ -1,9 +1,30 @@
 package com.bordza.booking.bordzaBooking.rest;
 
+import com.bordza.booking.bordzaBooking.domain.ClientEntity;
+import com.bordza.booking.bordzaBooking.domain.CourseEntity;
+import com.bordza.booking.bordzaBooking.repositories.CourseRepository;
+import com.bordza.booking.bordzaBooking.repositories.LocationRepository;
+import com.bordza.booking.bordzaBooking.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CourseRestController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @GetMapping("/api/allCourses")
+    public List<CourseEntity> showAllCourses(){
+
+        return courseRepository.findAll();
+    }
 
 
 
