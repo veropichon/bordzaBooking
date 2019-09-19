4e8faf00386b804fd4c4f1beeea30730282c644f
manza33 <bourgois.laetitia@gmail.com>
2019-09-19 15:30:03 +0200

    ok avant pull Valerian

--- src/main/java/com/bordza/booking/bordzaBooking/controllers/HeaderController.java
+++ src/main/java/com/bordza/booking/bordzaBooking/controllers/HeaderController.java
@@ -4,13 +4,18 @@ import com.bordza.booking.bordzaBooking.domain.CourseClientEntity;
 import com.bordza.booking.bordzaBooking.domain.CourseEntity;
 import com.bordza.booking.bordzaBooking.repositories.CourseClientRepository;
 import com.bordza.booking.bordzaBooking.repositories.CourseRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
+//@Controller
 public class HeaderController {
 
     @Autowired
@@ -27,16 +32,16 @@ public class HeaderController {
         CourseEntity courseEntity = new CourseEntity();
         CourseClientEntity courseClientEntity = new CourseClientEntity();
 
-        Integer CourseToValid = courseRepository;
-        Integer CourseToValid = ;
+        //Integer CourseToValid = courseRepository;
+        Integer BookingToValid = courseClientRepository.findAllByBkValidated(true).size();
+        log.info("BookingToValid : " + BookingToValid);
 
-
-        bookingsAndCoursesToValidate.put("modelCourseToValid",  );
-        bookingsAndCoursesToValidate.put("modelBookingToValid",  );
+        //bookingsAndCoursesToValidate.put("modelCourseToValid",  );
+        bookingsAndCoursesToValidate.put("modelBookingToValid", BookingToValid );
 
         model.addAllAttributes(bookingsAndCoursesToValidate);
 
-        return bookingsAndCoursesToValidate;
+        return "/general";
     }
 
 }
--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -1,22 +1,12 @@
 function cliAge(input_date) {
     var date_array = input_date.split("-");
 
-    log.info("cliAge dateArray : " + date_array);
-
-    log.info("cliAge inputDate : " + input_date);
-
     var date = new Date();
     date.setYear(date_array[0]);
     date.setMonth(date_array[1] - 1); // month démarre à 0
     date.setDate(date_array[2]);
 
     var age = Math.floor(((new Date).getTime() - date.getTime()) / (365.24 * 24 * 3600 * 1000));
-
-    viewTutor(age)
-
-}
-
-function viewTutor(age){
 
     if (age < 18) {
         document.getElementById("tutor").style.display = "block";
