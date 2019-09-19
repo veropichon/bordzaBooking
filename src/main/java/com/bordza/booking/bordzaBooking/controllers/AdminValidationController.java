f120de1d8122b7b1bf5ccabb9c6a42625a252e60
Veronique Pichon <name>
2019-09-19 11:54:21 +0200

    validation et suppression booking

--- src/main/java/com/bordza/booking/bordzaBooking/controllers/AdminValidationController.java
+++ src/main/java/com/bordza/booking/bordzaBooking/controllers/AdminValidationController.java
@@ -9,15 +9,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
 @Controller
-public class ValidationController {
+public class AdminValidationController {
 
     @Autowired
     CourseRepository courseRepository;
@@ -45,45 +46,86 @@ public class ValidationController {
 
     private static final Logger log = LoggerFactory.getLogger("log ValidationController");
 
-    @RequestMapping("/adminWaiting")
+    @GetMapping("/adminWaiting")
     public String validation(Model model) {
+        List<CourseClientEntity> courseClientsList = courseClientRepository.findAllByBkValidated(false);
 
-       // CourseClientEntity booking = courseClientRepository.findById(1L).get();
-        //model.addAttribute("modelCourseClient", booking);
+     /*   if (courseClientsList.size() == 0) {
+            String message = "Validation en attente ";
+            String message1 = "Toutes vos validations sont effectuées";
+            model.addAttribute("pageTitle", message);
+            model.addAttribute("nbvalidation", courseClientsList.size());
+            model.addAttribute("information", message1);
 
-        List<CourseClientEntity> courseClientsList = courseClientRepository.findAll();
-       // List<CourseClientEntity> courseClientsList = courseClientRepository.findAllbybkequalsfalse();
+
+            return "adminWaiting";
+
+        }
+     */
+
         model.addAttribute("courseClientsList" , courseClientsList);
+
+        CourseClientEntity courseClientEntity = new CourseClientEntity();
+        courseClientEntity.defaultValue(courseClientEntity);
+        model.addAttribute("modelCourseClient", courseClientEntity);
 
         List<ClientEntity> clientsList = clientRepository.findAll();
         model.addAttribute("clientsList", clientsList);
         log.info("Taille liste booking : " + courseClientsList.size());
+
         String message = "Validation en attente (" + courseClientsList.size() + ")";
         model.addAttribute("pageTitle", message);
-
-        //model.addAttribute("pageTitle", "Validation en attente ' ");
 
         return "adminWaiting";
     }
 
-    @RequestMapping("/adminSummary")
+    @GetMapping("/adminSummary")
     public String summary(Model model) {
 
         CourseEntity course = courseRepository.findById(1L).get();
         model.addAttribute("modelCourse", course);
-
-        List<CourseClientEntity> courseClientsList = courseClientRepository.findAll();
+    //  A revoir
+        log.info("id course : " + course.getCrsId());
+        List<CourseClientEntity> courseClientsList = courseClientRepository.findAllByBkId( course.getCrsId());
         model.addAttribute("courseClientsList" , courseClientsList);
-
-
-        /*log.info("course.getCrsFromDate() : " + course.getCrsFromDate());
-        log.info("bean FromTime : " + someBean.getFromTime());
-
-        log.info("bean FromDateTime : " + someBean.getFromDateTime());
-        */
-
         model.addAttribute("pageTitle", "Récapitulatif");
 
         return "adminSummary";
     }
+
+    @RequestMapping("/adminValidation")
+    public String validationBooking (Model model, @RequestParam Long bookingId) {
+        log.info("bookingId : " + bookingId);
+        CourseClientEntity courseClient = courseClientRepository.findById(bookingId).get();
+        courseClient.setBkValidated(true);
+        courseClientRepository.save(courseClient);
+
+        return "redirect:/adminWaiting";
+    }
+
+    @RequestMapping("/adminDelete")
+    public String deleteBooking (Model model, @RequestParam Long bookingId) {
+        log.info("bookingId : " + bookingId);
+        CourseClientEntity courseClient = courseClientRepository.findById(bookingId).get();
+        courseClientRepository.delete(courseClient);
+        return "redirect:/adminWaiting";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
